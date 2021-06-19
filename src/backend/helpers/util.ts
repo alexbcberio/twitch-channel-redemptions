3ffbd156cdde113f701ab0b76058ba6f68144594
alexbcberio <alexbcberio@gmail.com>
2021-06-19 23:00:41 +0200

    🏷️ Add type for Environment mode and remove raw environment export

--- src/backend/helpers/util.ts
+++ src/backend/helpers/util.ts
@@ -1,9 +1,10 @@
+import { Environment } from "../../enums/Environment";
+
 const environment = process.env.NODE_ENV;
-const isDevelopment = environment === "development";
-const isProduction = environment === "production";
+const isDevelopment = environment === Environment.development;
+const isProduction = environment === Environment.production;
 
 export {
-  environment,
   isDevelopment,
   isProduction
 };
---
+++ src/enums/Environment.ts
@@ -0,0 +1,4 @@
+export enum Environment {
+  development = "development",
+  production = "production"
+}
