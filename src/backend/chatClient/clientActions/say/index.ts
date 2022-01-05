f1ea2a1b64c82326edba2ee037a8102c89feccf6
alexbcberio <alexbcberio@gmail.com>
2022-01-06 00:44:01 +0100

    ✨ Add prefix and prevent exceeding message length

--- src/backend/chatClient/clientActions/say/index.ts
+++ src/backend/chatClient/clientActions/say/index.ts
@@ -1,7 +1,18 @@
 import { chatClient } from "../..";
 
-// send a chat message
+const maxMessageLength = 500;
+
 async function say(channel: string, message: string): Promise<void> {
+	message = `MrDestructoid ${message}`;
+
+	if (message.length > 500) {
+		const suffix = "...";
+		message = `${message.substring(
+			0,
+			maxMessageLength - suffix.length
+		)}${suffix}`;
+	}
+
 	await chatClient.say(channel, message);
 }
 
