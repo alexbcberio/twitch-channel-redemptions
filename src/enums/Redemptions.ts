1cd43db83ca2e798071518d888979cd7e03c32c5
alexbcberio <alexbcberio@gmail.com>
2022-01-06 03:23:15 +0100

    🏷️ Update reward ids

--- src/enums/Redemptions.ts
+++ src/enums/Redemptions.ts
@@ -1,9 +1,9 @@
 export enum RedemptionIds {
-	KaraokeTime = "27faa7e4-f496-4e91-92ae-a51f99b9e854",
-	RussianRoulette = "a73247ee-e33e-4e9b-9105-bd9d11e111fc",
-	TimeoutFriend = "638c642d-23d8-4264-9702-e77eeba134de",
-	HighlightMessage = "a26c0d9e-fd2c-4943-bc94-c5c2f2c974e4",
-	GetVip = "a215d6a0-2c11-4503-bb29-1ca98ef046ac",
-	StealVip = "ac750bd6-fb4c-4259-b06d-56953601243b",
-	Hidrate = "232e951f-93d1-4138-a0e3-9e822b4852e0"
+	GetVip = "ddcb6566-6b02-447c-a396-6b93ade3b480",
+	Hidrate = "3dfb7ed4-b696-4dff-92d7-18f9bfa3dd4a",
+	HighlightMessage = "df93120a-9ebe-493c-91e1-42338400dc35",
+	KaraokeTime = "53c72f19-61ae-4a27-bbe0-c8100e2fce8e",
+	RussianRoulette = "c38a6de3-ae92-4add-bd56-c6346c19ea93",
+	StealVip = "84283c28-a2c2-4da6-99c2-b662417b9d49",
+	TimeoutFriend = "1a6bd16d-2882-4125-9f8a-e3c7194ea8b6"
 }
