188d7303189e1376988823c2cb8604ca09759f84
Alexander BC <alexbcberio@gmail.com>
2022-05-14 01:36:34 +0200

    refactor(frontend): migrate card animations to animate.css

--- src/www/ts/actions/createCard.ts
+++ src/www/ts/actions/createCard.ts
@@ -1,5 +1,8 @@
+import "animate.css";
+
 import * as tinycolor from "tinycolor2";
 
+import { animate } from "../helpers/animate.css";
 import { createImg } from "../helpers/createImg";
 import { createText } from "../helpers/createText";
 import { insertCssVariables } from "../helpers/insertCssVariables";
@@ -13,83 +16,79 @@ export function createCard(
   const maxMessageLength = 120;
   const darkenLighten = 10;
 
-  return new Promise((res) => {
-    const card = document.createElement("div");
-    card.classList.add("card", "open");
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-    const img = createImg(image);
-    img.classList.add("card-image");
-    card.appendChild(img);
+  const img = createImg(image);
+  img.classList.add("card-image");
+  card.appendChild(img);
 
-    const body = document.createElement("div");
-    body.classList.add("card-body");
-    card.appendChild(body);
+  const body = document.createElement("div");
+  body.classList.add("card-body");
+  card.appendChild(body);
 
-    const titl = document.createElement("h1");
-    titl.classList.add("title");
-    titl.innerText = title;
-    body.appendChild(titl);
+  const titl = document.createElement("h1");
+  titl.classList.add("title");
+  titl.innerText = title;
+  body.appendChild(titl);
 
-    if (message.length > maxMessageLength) {
-      while (message.length > maxMessageLength) {
-        // eslint-disable-next-line no-magic-numbers
-        message = message.split(" ").slice(0, -1).join(" ");
-      }
-      message += "...";
+  if (message.length > maxMessageLength) {
+    while (message.length > maxMessageLength) {
+      // eslint-disable-next-line no-magic-numbers
+      message = message.split(" ").slice(0, -1).join(" ");
     }
+    message += "...";
+  }
 
-    const msg = createText(message);
-    msg.classList.add("message");
-    body.appendChild(msg);
+  const msg = createText(message);
+  msg.classList.add("message");
+  body.appendChild(msg);
 
-    let color = tinycolor(hexColor);
+  let color = tinycolor(hexColor);
 
-    if (!color.isValid()) {
-      color = tinycolor("#2EC90C");
-    }
+  if (!color.isValid()) {
+    color = tinycolor("#2EC90C");
+  }
 
-    const backgroundColor = color;
-    const borderColor = new tinycolor(backgroundColor.toHexString());
+  const backgroundColor = color;
+  const borderColor = new tinycolor(backgroundColor.toHexString());
 
-    if (backgroundColor.isLight()) {
-      card.classList.add("card-light");
-      borderColor.darken(darkenLighten).toHexString();
-    } else {
-      borderColor.lighten(darkenLighten).toHexString();
-    }
+  if (backgroundColor.isLight()) {
+    card.classList.add("card-light");
+    borderColor.darken(darkenLighten).toHexString();
+  } else {
+    borderColor.lighten(darkenLighten).toHexString();
+  }
 
-    insertCssVariables({
-      "--card-background-color": backgroundColor.toHexString(),
-      "--card-border-color": borderColor.toHexString(),
-    });
+  insertCssVariables({
+    "--card-background-color": backgroundColor.toHexString(),
+    "--card-border-color": borderColor.toHexString(),
+  });
 
-    card.addEventListener("animationend", () => {
-      if (card.classList.contains("open")) {
-        card.classList.remove("open");
+  return new Promise(async (res) => {
+    img.onload = async () => {
+      document.body.appendChild(card);
 
-        const wordsReadPerSecond = 3;
-        const minSecondsOpen = 4;
-        const maxSecondsOpen = 8;
+      await animate(card, "slideInDown");
 
-        const fairTime = message.split(" ").length / wordsReadPerSecond;
-        const timeOpen = Math.min(
-          Math.max(fairTime, minSecondsOpen),
-          maxSecondsOpen
-        );
+      const wordsReadPerSecond = 3;
+      const minSecondsOpen = 4;
+      const maxSecondsOpen = 8;
 
-        const msPerSecond = 1e3;
+      const fairTime = message.split(" ").length / wordsReadPerSecond;
+      const timeOpen = Math.min(
+        Math.max(fairTime, minSecondsOpen),
+        maxSecondsOpen
+      );
 
-        setTimeout(() => {
-          card.classList.add("close");
-        }, timeOpen * msPerSecond);
-      } else {
+      const msPerSecond = 1e3;
+
+      setTimeout(async () => {
+        await animate(card, "slideOutUp");
+
         card.remove();
         res();
-      }
-    });
-
-    img.onload = () => {
-      document.body.appendChild(card);
+      }, timeOpen * msPerSecond);
     };
   });
 }
