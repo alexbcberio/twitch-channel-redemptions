150ba3b5583cd480ae3429bb9a8b04205ad02628
Alexander BC <alexbcberio@gmail.com>
2022-05-13 21:54:57 +0200

    feat(frontend): improve audio player overlay
    Added albumart slide animation

--- src/www/ts/actions/updateSong.ts
+++ src/www/ts/actions/updateSong.ts
@@ -1,27 +1,101 @@
-export function updateSong(song: any) {
-  const { title, artist, coverArt } = song;
-  const playing = document.getElementById("playing");
+import { Song } from "../../../interfaces/Song";
 
-  if (!playing) {
+const hideTimeoutSeconds = 5e3;
+
+let hideTimeout: NodeJS.Timeout;
+let previousData: string | null = null;
+
+async function preloadImage(url: string): Promise<string> {
+  const data = await fetch(url);
+  const blob = await data.blob();
+
+  const reader = new FileReader();
+
+  return new Promise(async (res, rej) => {
+    reader.onerror = rej;
+    reader.onloadend = () => res(reader.result as string);
+    reader.readAsDataURL(blob);
+  });
+}
+
+// TODO: show player with animation
+function showPlayer(player: HTMLElement) {
+  player.style.display = "";
+}
+
+// TODO: hide player with animation
+function hidePlayer(player: HTMLElement) {
+  player.style.display = "none";
+
+  previousData = null;
+}
+
+async function setData(
+  player: HTMLElement,
+  data: Song,
+  skipAnimation: boolean = false
+) {
+  const { title, artist, coverArt } = data.song;
+  const [previousCoverArt, newCoverArt] =
+    player.querySelectorAll<HTMLDivElement>(".coverArt");
+  const trackName = player.querySelector<HTMLElement>(".trackName");
+  const trackArtist = player.querySelector<HTMLElement>(".trackArtist");
+
+  if (!previousCoverArt || !newCoverArt || !trackName || !trackArtist) {
     return;
   }
 
-  if (!title && !artist) {
-    playing.style.display = "none";
-    return;
-  }
+  const coverArtValue = skipAnimation ? coverArt : await preloadImage(coverArt);
+  const coverArtPropertyValue = `url(${coverArtValue})`;
 
-  playing.style.display = "";
-
-  const trackCoverArt = playing.querySelector<HTMLDivElement>(".coverArt");
-  const trackName = playing.querySelector<HTMLElement>(".trackName");
-  const trackArtist = playing.querySelector<HTMLElement>(".trackArtist");
-
-  if (!trackCoverArt || !trackName || !trackArtist) {
-    return;
-  }
-
-  trackCoverArt.style.setProperty("--coverArt", `url("${coverArt}")`);
+  newCoverArt.style.setProperty("--coverArt", coverArtPropertyValue);
   trackName.innerText = title;
   trackArtist.innerText = artist;
+
+  if (skipAnimation) {
+    previousCoverArt.style.setProperty("--coverArt", coverArtPropertyValue);
+    return;
+  }
+
+  previousCoverArt.onanimationend = () => {
+    previousCoverArt.style.setProperty("--coverArt", coverArtPropertyValue);
+    previousCoverArt.classList.remove("previous");
+    newCoverArt.classList.remove("next");
+  };
+
+  previousCoverArt.classList.remove("previous");
+  newCoverArt.classList.remove("next");
+  previousCoverArt.classList.add("previous");
+  newCoverArt.classList.add("next");
+}
+
+export async function updateSong(data: Song) {
+  const { title, artist } = data.song;
+  const player = document.getElementById("playing");
+
+  if (!player) {
+    return;
+  }
+
+  clearTimeout(hideTimeout);
+
+  if (!title && !artist) {
+    hideTimeout = setTimeout(() => hidePlayer(player), hideTimeoutSeconds);
+    return;
+  }
+
+  const dataString = JSON.stringify(data);
+
+  // verify data is of a new song
+  if (previousData === dataString) {
+    return;
+  }
+
+  if (!previousData) {
+    showPlayer(player);
+  }
+
+  await setData(player, data, previousData === null);
+
+  previousData = dataString;
 }
