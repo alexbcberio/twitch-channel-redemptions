cc3b816784d9078fd078feb95b5684c9a71a6a7b
Alexander BC <alexbcberio@gmail.com>
2022-12-04 18:31:35 +0100

    feat(app): add availableSubscriptionTypesWithScope twitch helper method
    returns all the subscription event types that can be subscribed to given an array of api and chatPubSub scopes

--- src/backend/helpers/twitch.ts
+++ src/backend/helpers/twitch.ts
@@ -4,6 +4,11 @@ import {
   HelixCreateCustomRewardData,
   UserIdResolvable,
 } from "@twurple/api";
+import {
+  ApiScope,
+  ChatPubSubScope,
+  SubscriptionTypeV1 as SubscriptionType,
+} from "../../enums/EventSub";
 import { error, extendLogger } from "./log";
 import { getTokenData, saveTokenData } from "./tokenData";
 
@@ -139,6 +144,90 @@ async function cancelRewards(
   );
 }
 
+function availableSubscriptionTypesWithScope(
+  scopes: Array<ApiScope | ChatPubSubScope>
+) {
+  const subscriptionTypes = Array<SubscriptionType>(
+    SubscriptionType.ChannelFollow,
+    SubscriptionType.ChannelRaid,
+    SubscriptionType.StreamOnline,
+    SubscriptionType.StreamOffline
+  );
+
+  for (let i = 0; i < scopes.length; i++) {
+    switch (scopes[i]) {
+      case ApiScope.ChannelReadSubscriptions:
+        subscriptionTypes.push(
+          SubscriptionType.ChannelSubscribe,
+          SubscriptionType.ChannelSubscriptionEnd,
+          SubscriptionType.ChannelSubscriptionGift,
+          SubscriptionType.ChannelSubscriptionMessage
+        );
+        break;
+      case ApiScope.BitsRead:
+        subscriptionTypes.push(SubscriptionType.ChannelCheer);
+        break;
+      case ChatPubSubScope.ChannelModerate:
+        subscriptionTypes.push(
+          SubscriptionType.ChannelBan,
+          SubscriptionType.ChannelUnban
+        );
+        break;
+      case ApiScope.ModerationRead:
+        subscriptionTypes.push(
+          SubscriptionType.ChannelModeratorAdd,
+          SubscriptionType.ChannelModeratorRemove
+        );
+        break;
+      case ApiScope.ChannelReadRedemptions:
+      case ApiScope.ChannelManageRedemptions:
+        subscriptionTypes.push(
+          SubscriptionType.ChannelChannelPointsCustomRewardAdd,
+          SubscriptionType.ChannelChannelPointsCustomRewardUpdate,
+          SubscriptionType.ChannelChannelPointsCustomRewardRemove,
+          SubscriptionType.ChannelChannelPointsCustomRewardRedemptionAdd,
+          SubscriptionType.ChannelChannelPointsCustomRewardRedemptionUpdate
+        );
+        break;
+      case ApiScope.ChannelReadPolls:
+      case ApiScope.ChannelManagePolls:
+        subscriptionTypes.push(
+          SubscriptionType.ChannelPollBegin,
+          SubscriptionType.ChannelPollProgress,
+          SubscriptionType.ChannelPollEnd
+        );
+        break;
+      case ApiScope.ChannelReadPredictions:
+      case ApiScope.ChannelManagePredictions:
+        subscriptionTypes.push(
+          SubscriptionType.ChannelPredictionBegin,
+          SubscriptionType.ChannelPredictionProgress,
+          SubscriptionType.ChannelPredictionLock,
+          SubscriptionType.ChannelPredictionEnd
+        );
+        break;
+      case ApiScope.ChannelReadHypeTrain:
+        subscriptionTypes.push(
+          SubscriptionType.ChannelHypeTrainBegin,
+          SubscriptionType.ChannelHypeTrainProgress,
+          SubscriptionType.ChannelHypeTrainEnd
+        );
+        break;
+      case ApiScope.ChannelReadGoals:
+        subscriptionTypes.push(
+          SubscriptionType.ChannelGoalBegin,
+          SubscriptionType.ChannelGoalProgress,
+          SubscriptionType.ChannelGoalEnd
+        );
+        break;
+      default:
+    }
+  }
+
+  // remove duplicated events
+  return [...new Set(subscriptionTypes)];
+}
+
 export {
   getAuthProvider,
   getApiClient,
@@ -147,4 +236,5 @@ export {
   completeRewards,
   cancelRewards,
   createReward,
+  availableSubscriptionTypesWithScope,
 };
