f1470888e20ae7403cb904113b7a137c186025d9
Alexander BC <alexbcberio@gmail.com>
2023-04-07 18:16:27 +0200

    refactor(action): rename timeoutFriends directory to timeoutFriend

--- src/backend/eventSubClient/events/channelPointsCustomRewardRedemptionAdd/actions/index.ts
+++ src/backend/eventSubClient/events/channelPointsCustomRewardRedemptionAdd/actions/index.ts
@@ -4,4 +4,4 @@ export * from "./hidrate";
 export * from "./hightlightMessage";
 export * from "./russianRoulette";
 export * from "./stealVip";
-export * from "./timeoutFriends";
+export * from "./timeoutFriend";
