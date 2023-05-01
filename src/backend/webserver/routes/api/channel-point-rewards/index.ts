0e3cbca84440c005d5dc1e1398adf7db7e06a531
Alexander BC <alexbcberio@gmail.com>
2023-05-01 11:28:40 +0200

    feat(webserver): create route to obtain channel point rewards

---
+++ src/backend/webserver/routes/api/channel-point-rewards/index.ts
@@ -0,0 +1,34 @@
+import { getApiClient, getStreamerUser } from "../../../../helpers/twitch";
+
+import { FastifyInstance } from "fastify";
+
+export default function (server: FastifyInstance) {
+  server.route({
+    method: "GET",
+    url: "/",
+    async handler(_req, reply) {
+      const apiClient = await getApiClient();
+      const user = await getStreamerUser();
+
+      const rawRewards = await apiClient.channelPoints.getCustomRewards(
+        user.id
+      );
+      const rewardImageScale = 4;
+
+      const rewards = rawRewards.map((reward) => {
+        return {
+          id: reward.id,
+          title: reward.title,
+          cost: reward.cost,
+          color: reward.backgroundColor,
+          image: reward.getImageUrl(rewardImageScale),
+          enabled: reward.isEnabled,
+        };
+      });
+
+      return reply.send(rewards.sort((a, b) => a.cost - b.cost));
+    },
+  });
+
+  return Promise.resolve();
+}
